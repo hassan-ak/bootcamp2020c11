dc12e142937229c574495c68f585eef4e704c8f8
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-11-08 18:24:55 +0500

    Install and setup React Router
    1:- Installed React routers using "npm install react-router-dom@6.0.0-beta.0"
    2:- Update index.js to make Router available for every  App component.
    3:- Create Home.js, About.js and Products.js for respective pages
    4:- Updated App.js to define routes
    5:- Updated App.js to define links
    6:- Updated app.js to define route and link for product page and while defining route also defined it for product id
    7:- Updated Products.js for useParams hooks
    8:- Updated App.js to define links for product types (mobile/laptop)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { Link, Route, Routes } from 'react-router-dom';
+import { About } from './About';
 import './App.css';
+import { Error } from './Error';
+import { Home } from './Home';
+import { Products } from './Products';
 
 function App() {
   return (
     <div>
-      Hello
+      <h1>WellCome</h1>
+      <div>
+        <Link to="about">About</Link>
+        {"    "}
+        {"    "}
+        <Link to="home">Home</Link>
+        {"    "}
+        {"    "}
+        <Link to="products">Products</Link>
+        {"    "}
+        {"    "}
+        <Link to="products/mobile">Mobile</Link>
+        {"    "}
+        {"    "}
+        <Link to="products/laptop">Laptop</Link>
+      </div>
+      <Routes>
+        <Route path="/"></Route>
+        <Route path="about" element={<About></About>}></Route>
+        <Route path="home" element={<Home></Home>}></Route>
+        <Route path="products/:productId" element={<Products></Products>}></Route>
+        <Route path="*" element={<Error></Error>}></Route>
+      </Routes>
     </div>
   );
 }
