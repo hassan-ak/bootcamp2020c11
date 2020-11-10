b6570c4b96465e1cf0d35c9f4fbd2bb905978f7b
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-11-10 18:16:18 +0500

    Update Home.js
    Updated Home.js, Home.css and App.css and deleted un-necessary files

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import { WelCome } from './Welcome/WelCome';
-import Proceed from './Proceed/Proceed'
+import title from '../../Images/title.png';
 import { Link } from 'react-router-dom'
-import styles from './Home.module.css';
+import './Home.css';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
+const useStyles = makeStyles((theme) => ({
+  button: {
+    borderRadius: "50px",
+    textDecoration: "none",
+  },
+}));
 
 export const Home= () => {
+
+  const classes = useStyles();
+
   return (
-    <div className = {styles.container}>
-      <WelCome/>
-      <Link className = {styles.links} to="products" ><Proceed/></Link>
+    <div className = "container">
+
+      <div className = "shoeContainer">
+            <h1>Wellcome to</h1>
+            <img src={title} alt="Shoes" className="shoeImage"/>
+            <h1>Store</h1>
+        </div>
+
+      <Link to="products" className="link">
+        <Button
+          variant="contained"
+          color="white"
+          className={classes.button}
+          endIcon={<ArrowForwardIcon/>}>
+            Proceed to store
+        </Button>
+      </Link>
+
     </div>
   );
 }
