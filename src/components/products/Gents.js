b109ef9b130cc9287c0e01fbda22dc9461cf4f25
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-11-11 15:13:15 +0500

    Removed Errors

--- src/components/products/Gents.js
+++ src/components/products/Gents.js
@@ -12,7 +12,6 @@ import { Grid } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 import Header from '../header/Header'
-import Footer from '../footer/Footer'
 
 const useStyles = makeStyles({
     root: {
